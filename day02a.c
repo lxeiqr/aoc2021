473dbc9b2e8adeaffd56d6410402124a04779d76
lxeiqr <lxeiqr@protonmail.com>
2021-12-02 10:34:42 +0100

    Day 02a

---
+++ day02a.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+enum MoveType {
+    MOVE_HORZ,
+    MOVE_DEPTH,
+};
+
+bool parse_cmd(char *in, enum MoveType *mvtype, int *value) {
+    char *token;
+    bool reverse = false;
+    token = strtok(in, " ");
+
+    if(!strcmp(in, "forward")) {
+        *mvtype = MOVE_HORZ;
+    } else if(!strcmp(in, "down")) {
+        *mvtype = MOVE_DEPTH;
+    } else if(!strcmp(in, "up")) {
+        reverse = true;
+        *mvtype = MOVE_DEPTH;
+    } else {
+        return true;
+    }
+
+    token = strtok(NULL, " ");
+    *value = atoi(token);
+
+    if(reverse)
+        *value = -(*value);
+    
+    return false;
+}
+
+int main() {
+    size_t input_n = 128;
+    char input[128];
+
+    int depth, horizontal;
+
+    while(fgets(input, input_n, stdin) != NULL) {
+        enum MoveType mvtype;
+        int value;
+
+        if(parse_cmd(input, &mvtype, &value)) {
+            fprintf(stderr, "Couldn't parse: %s\n", input);
+            exit(1);
+        }
+
+        if(mvtype == MOVE_HORZ)
+            horizontal += value;
+        else
+            depth += value;
+    }
+
+    printf("%d", horizontal * depth);
+}
